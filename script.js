6e2977dcabbf93848493c2d90b804220dd616e4d
Uzain <uzainmalik666@gmail.com>
2023-04-10 16:44:41 -0700

    added a lot of logic

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-let fieldErrors = document.querySelector('.field-errors');
-let dateErrors = document.querySelector('.date-errors');
-let submitBtn = document.getElementById('submit-btn');
-let allInputs = document.getElementsByTagName('input');
-let allLabels = document.getElementsByTagName('label')
+let fieldErrors = document.querySelector(".field-errors");
+let dateErrors = document.querySelector(".date-error");
+let submitBtn = document.getElementById("submit-btn");
+let allInputs = document.getElementsByTagName("input");
+let allLabels = document.getElementsByTagName("label");
 
-submitBtn.addEventListener('click', () => {
-    let inputDayVal = document.getElementById('input-day').value;
-    let inputMonthVal = document.getElementById('input-month').value;
-    let inputYearVal = document.getElementById('input-year').value;
-    if(inputDayVal == '' || inputMonthVal == '' || inputYearVal == ''){
-        for(let i = 0; i < allInputs.length; i++) {
-            allInputs[i].style.borderColor = 'hsl(0, 100%, 67%)'; 
-            allLabels[i].style.color = 'hsl(0, 100%, 67%)'; 
-        }
-        fieldErrors.style.display = "flex"
+submitBtn.addEventListener("click", () => {
+  let inputDayVal = document.getElementById("input-day").value;
+  let inputMonthVal = document.getElementById("input-month").value;
+  let inputYearVal = document.getElementById("input-year").value;
+
+  let newDate = new Date();
+  let currentYear = newDate.getFullYear();
+  let allDays = "31";
+  let allMonths = "12";
+
+  for (let i = 0; i < allInputs.length; i++) {
+    allInputs[i].style.borderColor = "hsl(0, 0%, 86%)";
+    allLabels[i].style.color = "hsl(0, 1%, 44%)";
+  }
+  fieldErrors.style.display = "none";
+  dateErrors.style.display = "none";
+
+  if (inputDayVal == "" || inputMonthVal == "" || inputYearVal == "") {
+    console.log("how");
+    for (let i = 0; i < allInputs.length; i++) {
+      allInputs[i].style.borderColor = "hsl(0, 100%, 67%)";
+      allLabels[i].style.color = "hsl(0, 100%, 67%)";
     }
-})
+    dateErrors.style.display = "none";
+    fieldErrors.style.display = "flex";
+  } 
+});
