94faa02c19ebff5d221a31e0ef5a663c290b2cf7
Uzain <uzainmalik666@gmail.com>
2023-04-10 17:22:03 -0700

    done the logic

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ submitBtn.addEventListener("click", () => {
   let inputYearVal = document.getElementById("input-year").value;
 
   let newDate = new Date();
-  let currentYear = newDate.getFullYear();
+  let currentYear = newDate.getFullYear(); 
   let allDays = "31";
   let allMonths = "12";
 
@@ -22,7 +22,6 @@ submitBtn.addEventListener("click", () => {
   dateErrors.style.display = "none";
 
   if (inputDayVal == "" || inputMonthVal == "" || inputYearVal == "") {
-    console.log("how");
     for (let i = 0; i < allInputs.length; i++) {
       allInputs[i].style.borderColor = "hsl(0, 100%, 67%)";
       allLabels[i].style.color = "hsl(0, 100%, 67%)";
@@ -30,4 +29,36 @@ submitBtn.addEventListener("click", () => {
     dateErrors.style.display = "none";
     fieldErrors.style.display = "flex";
   } 
+  if(inputDayVal > allDays && inputMonthVal > allMonths && inputYearVal > currentYear){
+    for (let i = 0; i < allInputs.length; i++) {
+        allInputs[i].style.borderColor = "hsl(0, 100%, 67%)";
+        allLabels[i].style.color = "hsl(0, 100%, 67%)";
+      }
+      dateErrors.style.display = "flex";
+      fieldErrors.style.display = "none";
+  }
+
+  const calculateAge = () => {
+    let currentDay = newDate.getDate();
+    let currentMonth = 1 + newDate.getMonth();
+    let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  
+    if(inputDayVal > currentDay){
+      currentDay += month[currentMonth - 1];
+      currentMonth -= 1;
+    }
+    if(inputMonthVal > currentMonth){
+      currentMonth += 12;
+      currentYear -= 1;
+    }
+    let calculatedDay = currentDay - inputDayVal;
+    let calculatedMonth = currentMonth - inputMonthVal;
+    let calculatedYear = currentYear - inputYearVal;
+
+    document.getElementsByTagName('span')[0].textContent = calculatedYear
+    document.getElementsByTagName('span')[1].textContent = calculatedMonth
+    document.getElementsByTagName('span')[2].textContent = calculatedDay
+  }
+
+  calculateAge();
 });
